2474dcc10d38d775bc6cecb6a11cbe0014adb670
thangnguyen1992 <ntthang4@cmcglobal.vn>
2024-12-28 14:14:18 +0700

    done

--- src/main/java/com/thang/account_service/client/INotificationService.java
+++ src/main/java/com/thang/account_service/client/INotificationService.java
@@ -1,11 +1,21 @@
 package com.thang.account_service.client;
 
 import com.thang.account_service.model.MessageDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-@FeignClient(name = "notification-service", url = "http://localhost:9084")
+@FeignClient(name = "notification-service", url = "http://localhost:9084", fallback = NotificationServiceImpl.class)
 public interface INotificationService {
     @PostMapping("/sendNotificationEmail.do")
     void sendNotificationEmail(@RequestBody MessageDTO messageDTO);
+}
+
+class NotificationServiceImpl implements INotificationService {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Override
+    public void sendNotificationEmail(MessageDTO messageDTO) {
+        logger.error("Notification service is slow");
+    }
 }
--- src/main/java/com/thang/account_service/client/IStatisticService.java
+++ src/main/java/com/thang/account_service/client/IStatisticService.java
@@ -1,12 +1,24 @@
 package com.thang.account_service.client;
 
 import com.thang.account_service.model.StatisticDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "statistic-service", url = "http://localhost:9082")
+@FeignClient(name = "statistic-service", url = "http://localhost:9082", fallback = NotificationServiceImpl.class)
 public interface IStatisticService {
     @PostMapping("/createNewStatistic.do")
     void createNewStatistic(@RequestBody StatisticDTO statisticDTO);
+}
+@Component
+class StatisticServiceImpl implements IStatisticService {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Override
+    public void createNewStatistic(StatisticDTO statisticDTO) {
+        //fallback
+        logger.error("Statistic service is slow");
+    }
 }
