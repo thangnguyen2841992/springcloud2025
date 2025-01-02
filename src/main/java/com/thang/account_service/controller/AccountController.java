1071d254a79c91ba8b7bf68c147598ef10944fc5
thangnguyen2841992 <ntthang4@cmcglobal.vn>
2025-01-02 21:48:44 +0700

    done

--- src/main/java/com/thang/account_service/controller/AccountController.java
+++ src/main/java/com/thang/account_service/controller/AccountController.java
@@ -6,12 +6,16 @@ import com.thang.account_service.model.AccountDTO;
 import com.thang.account_service.model.MessageDTO;
 import com.thang.account_service.model.StatisticDTO;
 import com.thang.account_service.service.IAccountService;
+import jakarta.annotation.security.PermitAll;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 
 @RestController
@@ -26,22 +30,43 @@ public class AccountController {
     INotificationService notificationService;
     Logger logger = LoggerFactory.getLogger(AccountController.class);
 
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @GetMapping("/getAllAccount.do")
     public List<AccountDTO> getAllAccount() {
         logger.info("Account Service Controller: get all accounts");
-        statisticService.createNewStatistic(new StatisticDTO("Get all accounts", new Date()));
+//        statisticService.createNewStatistic(new StatisticDTO("Get all accounts", new Date()));
         return accountService.getAllAccounts();
     }
-
+    //hasAuthority can chu ROLE_
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @GetMapping("/getAccDetail.do/{accountId}")
     public AccountDTO getAccDetail(@PathVariable("accountId") Long accountId) {
         return this.accountService.getAccountById(accountId);
     }
 
+    @PostMapping("/register.do")
+    @PermitAll
+    public AccountDTO register(@RequestBody AccountDTO accountDTO, @RequestHeader("Authorization") String bearerToken) {
+        accountDTO.setRoles(new HashSet<String>(List.of("ROLE_USER")));
+        addNewAcc(accountDTO);
+        return accountDTO;
+    }
+
     @PostMapping("/createNewAccount.do")
-    public AccountDTO createNewAccount(@RequestBody AccountDTO accountDTO) {
+    @PreAuthorize("hasAnyAuthority('SCOPE_read') && hasRole('ADMIN')")
+    public AccountDTO createNewAccount(@RequestBody AccountDTO accountDTO, @RequestHeader("Authorization") String bearerToken) {
+        addNewAcc(accountDTO);
+        return accountDTO;
+    }
+//hasRole bo chu ROLE_
+    @PreAuthorize("hasAnyAuthority('SCOPE_write') && hasRole('ADMIN')")
+    @DeleteMapping("/deleteAccount.do/{accountId}")
+    public void deleteAccount(@PathVariable("accountId") Long accountId) {
+        this.accountService.deleteAccount(accountId);
+    }
+    private void addNewAcc(AccountDTO accountDTO) {
         this.accountService.addAccount(accountDTO);
-        statisticService.createNewStatistic(new StatisticDTO("Account " + accountDTO.getUsername() +  " is created", new Date()));
+        statisticService.createNewStatistic(new StatisticDTO("Account " + accountDTO.getUsername() + " is created", new Date()));
         MessageDTO messageDTO = new MessageDTO();
         messageDTO.setFrom("nguyenthang29tbdl@gmail.com");
         messageDTO.setTo(accountDTO.getUsername()); //username is Email
@@ -50,12 +75,6 @@ public class AccountController {
         messageDTO.setContent("Cho tôi 10 tỷ nhé");
 
         notificationService.sendNotificationEmail(messageDTO);
-        return accountDTO;
-    }
-
-    @DeleteMapping("/deleteAccount.do/{accountId}")
-    public void deleteAccount(@PathVariable("accountId") Long accountId) {
-        this.accountService.deleteAccount(accountId);
     }
 
 }
